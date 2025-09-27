ee8d1779c97ce42f9bf2a4b84c395da140217015
brianc0710 <brianc0710@gmail.com>
2025-09-28 08:32:14 +1000

    cognito feature added

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const swaggerUI = require('swagger-ui-express');
-const env = require('dotenv')
+const env = require('dotenv');
 const authRoutes = require('./src/Route/AuthenticationRoute');
 const fileRoutes = require('./src/Route/FileRoute');
-const fs = require('fs');
 const path = require('path');
 const loadSwaggerDocument = require('./src/Function/swagger.js');
 const { getParameter } = require("./src/parameterStore.js");
+
 const app = express();
 
 env.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 const observationRoutes = require("./src/Route/ObservationRoute");
@@ -25,17 +24,17 @@ app.use('/api/file', fileRoutes);
 const swaggerDocument = loadSwaggerDocument();
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-//Parameter
-
+// ---- Parameter Store + Server Start ----
 (async () => {
-  const serviceUrl = await getParameter("/n10820566/cloubirding");
-  console.log("ClouBirding Service URL:", serviceUrl);
+  try {
+    const serviceUrl = await getParameter("/n10820566/cloubirding");
+    console.log("ClouBirding Service URL:", serviceUrl);
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to start app:", err);
+  }
 })();
