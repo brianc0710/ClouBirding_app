d18bebb0fc8a5693c67f4ebf8f76f2a197952727
brianc0710 <brianc0710@gmail.com>
2025-09-28 19:40:39 +1000

    cashe skeleton added

---
+++ src/cache/memcached.js
@@ -0,0 +1,15 @@
+const Memcached = require("memcached");
+const memcached = new Memcached("cloubirding-10820566-cache.km2jzi.cfg.apse2.cache.amazonaws.com:11211");
+
+// Promisify for async/await
+memcached.aGet = (key) =>
+  new Promise((resolve, reject) => {
+    memcached.get(key, (err, data) => (err ? reject(err) : resolve(data)));
+  });
+
+memcached.aSet = (key, value, ttl) =>
+  new Promise((resolve, reject) => {
+    memcached.set(key, value, ttl, (err) => (err ? reject(err) : resolve(true)));
+  });
+
+module.exports = memcached;
